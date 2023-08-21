fb6a19c05fbf6bb1f9cdf996f915e6bb1f0cd823
admin <nickpav1@outlook.com>
2023-08-21 15:48:10 +0300

    change README.md

--- README.md
+++ README.md
@@ -3,16 +3,31 @@
 - python 3.11+
 
 ## Install
+```sh
 pip install -r requirements.txt
+```
 
 ## Preparing database
+```sh
 python manage.py db create
+```
 
 ## Run
+```sh
 python manage.py runserver
+```
 
 ## Cli tool for task monitoring
+```sh
 python manage.py tasks show
+```
 
 ## Cli tool for task cleanup
+```sh
 python manage.py tasks cleanup
+```
+
+##Run tests
+```sh
+python test.py
+```
--- test.py
+++ test.py
@@ -3,13 +3,14 @@ import requests
 
 
 def main():
+    origin = 'http://127.0.0.1:5000'
     s = requests.Session()
-    files = {'file': open('data.csv', 'rb')}
-    resp = s.post('http://127.0.0.1:5000/api/calculateDistance', files=files)
+    files = {'file': open('test_data.csv', 'rb')}
+    resp = s.post(origin + '/api/calculateDistance', files=files)
     task_id = resp.json()['task_id']
     while True:
         time.sleep(1.0)
-        r = s.get('http://127.0.0.1:5000/api/getResults?task_id='+ task_id)
+        r = s.get('/api/getResults?task_id='+ task_id)
         data = r.json()
         print(data)
         if data["status"] == "done":
